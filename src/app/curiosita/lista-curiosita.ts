bd37f45dd05c34816fae7b545262cb69a63d74a4
Mr.Alsi <gabriel.alsina22@gmail.com>
2022-06-30 12:02:37 +0200

    Finito menù Baby Animal e sezione curiosità

--- src/app/curiosita/lista-curiosita.ts
+++ src/app/curiosita/lista-curiosita.ts
@@ -1,6 +1,7 @@
 import { Curiosita } from "./curiosita";
 
 export const CURIOSITA: Curiosita[] = [
-    {animale: "Leone", icona: "", img: "leone.png", descrizione: "Il leone è un gatto grande con i capelli lunghi", curiosita: ["Lorem ipsum", "secondo Lorem Ipsum"]},
-    {animale: "Zebra", icona: "", img: "zebra.png", descrizione: "La zebra è un cavallo a strisce", curiosita: ["Lorem ipsum"]},
+    {animale: "Leone", icona: "leone.jpg", img: "leone.jpg", descrizione: "Il leone è un gatto grande con i capelli lunghi", curiosita: ["Lorem ipsum", "secondo Lorem Ipsum"]},
+    {animale: "Zebra", icona: "zebra.jpg", img: "zebra.jpg", descrizione: "La zebra è un cavallo a strisce", curiosita: ["Lorem ipsum"]},
+    {animale: "Ape", icona: "ape.jpg", img: "ape.jpg", descrizione: "Le api non vanno in ape WTF", curiosita: ["Lorem ipsum"]},
 ]
