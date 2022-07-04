08e3a0020816f719c710a4b67ec9bcde23acfcf6
carlotta carboni <carlotta.carboni2@studio.unibo.it>
2022-07-04 17:20:46 +0200

    creazione professionisti

--- src/app/professionisti/professionisti.component.ts
+++ src/app/professionisti/professionisti.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-professionisti',
@@ -6,10 +9,61 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./professionisti.component.css']
 })
 export class ProfessionistiComponent implements OnInit {
+  form: FormGroup;
 
-  constructor() { }
+  on?: string;
+  dom?: string;
+  professionista?: string; //variabile per sapere che tipo di professionista sto creando
+  constructor(public fb: FormBuilder, private router: Router) { 
+    this.form = fb.group({
+      "nome": ['',Validators.required],
+      "città": ['',Validators.required],
+      "via": ['',Validators.required],
+      "civico": ['',Validators.required],
+      "servizio": ['',Validators.required],
+      "online": [this.on],
+      "domicilio": [this.dom],
+      "tipo": [this.professionista]
+      //disponibilità?
+    });
+  }
 
   ngOnInit(): void {
+  }
+
+
+  //metodo per prendere dal radio la tipologia di professionista
+  checked(prof: string): void{
+    this.professionista=prof;
+    console.log(this.professionista);
+  }
+
+  salva(): void{
+    if(!this.form.valid){
+      alert("dati mancati");
+      return;
+    }else{
+      if(this.on!=undefined && this.dom!=undefined){
+        alert("aggiunto professionista con successo");
+        //@todo aggiungerlo veramente al db
+        this.router.navigate(['newProfessionisti']);//perché non si refresha la pagina?
+      }else{
+        alert("compilare tutti i campi");
+        return;
+      }
+    }
+  }
+
+  //metodo per prendere il valore del radio per sapere se il professionista offre servizi a domicilio
+  domicilio(radio: string): void{
+    this.dom=radio;
+    console.log(this.dom)
+  }
+
+
+  //metodo per prendere il valore del radio per sapere se il professionista offre servizi online
+  online(radio: string): void{
+    this.on=radio;
   }
 
 }
