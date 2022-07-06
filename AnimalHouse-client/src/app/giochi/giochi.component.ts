ed2763ccbb256dac77e0ce224b658d7fc9351062
carlotta carboni <carlotta.carboni2@studio.unibo.it>
2022-07-06 17:51:17 +0200

    if giochi

--- AnimalHouse-client/src/app/giochi/giochi.component.ts
+++ AnimalHouse-client/src/app/giochi/giochi.component.ts
@@ -9,6 +9,9 @@ import { GIOCHI } from '../lista-giochi';
 export class GiochiComponent implements OnInit {  
   giochi = GIOCHI;
   giochiamo?: string;
+  memory?: boolean;
+  quiz?: boolean;
+  quantiSono?: boolean;
   constructor() { }
 
   ngOnInit(): void {
@@ -16,5 +19,22 @@ export class GiochiComponent implements OnInit {
 
   giocaA(gioco: string): void {
     this.giochiamo = gioco;
+    if(this.giochiamo=="Memory"){
+      this.memory= true;
+      this.quiz=false;
+      this.quantiSono= false;
+    }else{
+      if(this.giochiamo=="Quiz"){
+        this.memory= false;
+        this.quiz=true;
+        this.quantiSono= false;
+      }else{
+        this.memory= false;
+        this.quiz=false;
+        this.quantiSono= true;
+      }
+    }
   }
+
+
 }
