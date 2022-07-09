65557af45f391ebe49ed6ee994c163818c4aa8fe
carlotta carboni <carlotta.carboni2@studio.unibo.it>
2022-07-09 09:27:36 +0200

    connessione

--- AnimalHouse-server/connessioneDB.js
+++ AnimalHouse-server/connessioneDB.js
@@ -5,5 +5,5 @@ dotenv.config({ path: './.env'});
 const URI = process.env.URI;
 const mongo = new MongoClient(URI);
 const db = mongo.db("AnimalHouse");
-console.log("URI", URI)
+//console.log("URI", URI)
 module.exports = db;
