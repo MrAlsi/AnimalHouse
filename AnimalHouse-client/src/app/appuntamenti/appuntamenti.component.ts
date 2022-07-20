9f442bd75eebf442fb9b9bc17c2ac05a5483c254
carlotta carboni <carlotta.carboni2@studio.unibo.it>
2022-07-20 17:09:57 +0200

    barra di ricerca professionisti in appuntamenti

--- AnimalHouse-client/src/app/appuntamenti/appuntamenti.component.ts
+++ AnimalHouse-client/src/app/appuntamenti/appuntamenti.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, FormGroup} from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { ProfiloServiceService } from '../profilo-service.service';
+
+
+
 
 @Component({
   selector: 'app-appuntamenti',
@@ -7,9 +13,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppuntamentiComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup;
+  search: boolean= false;
+
+  constructor(public fb: FormBuilder, public http: HttpClient, public profilo: ProfiloServiceService) { 
+    this.form=fb.group({
+      "cerca": [""],
+    });
+  }
 
   ngOnInit(): void {
+    this.cercatutti(this.profilo.id);
+  }
+
+  cercatutti(id: any): void{
+    //@todo chiamata al db con tutti gli appuntamenti della persona
+    this.search=false;
+  }
+
+
+  cerca(): void{
+    if(this.form.value.cerca!=""){
+      //@todo chiamata al db dove cerco tra gli appuntamenti solo quelli con quel professionista
+      //guarda cerca in utenti
+    }else{
+      this.search=false;
+      this.cercatutti(this.profilo.id);
+    }
   }
 
 }
