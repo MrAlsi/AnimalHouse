4e6f96c45f14e31be7ac2d9182aa203e6bf992be
carlotta carboni <carlotta.carboni2@studio.unibo.it>
2022-07-30 10:46:59 +0200

    commento

--- AnimalHouse-server/routes/crud.js
+++ AnimalHouse-server/routes/crud.js
@@ -4,7 +4,7 @@ var express = require('express');
 var router = express.Router();
 const routers = require("../controller/crud")
 
-//CREATE: un documento ad una collection
+//CREATE: un documento in una collection
 router.put('/:collezione', routers.setData);
 
 //READ: tutti i dati da una collection
